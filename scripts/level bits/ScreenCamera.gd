bc80561161de8782254163f882792b76a3de9b5b
mochi-mitsu <sumikak7@gmail.com>
2024-03-12 18:33:09 +0000

    git update

---
+++ scripts/level bits/ScreenCamera.gd
@@ -0,0 +1,36 @@
+extends Camera2D
+
+# Camera script follwing a target (usually the player)
+# This camera is snapped to a grid, therefore only moves when the character exits a screen.
+
+@export var target : NodePath
+@export var align_time : float = 0.2
+@export var screen_size := Vector2(1920, 1080)
+@onready var Target = get_node_or_null(target)
+
+func _ready():
+	var limits = Rect2($"../TileMap".get_used_rect())
+	var cell_size = Vector2(16, 16)
+	self.limit_top = limits.position.y * cell_size.y
+	self.limit_bottom = limits.end.y * cell_size.y
+	self.limit_left = limits.position.x * cell_size.x
+	self.limit_right = limits.end.x * cell_size.x
+
+func _physics_process(_delta: float) -> void:
+	if not is_instance_valid(Target):
+		var targets: Array = get_tree().get_nodes_in_group("Player")
+		if targets: Target = targets[0]
+	if not is_instance_valid(Target):
+		return
+	
+	# Actual movement
+	#var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
+	#tween.tween_property(self, "global_position", Vector2i(Target.position.x, Target.position.y), align_time)
+	if floor(self.position.distance_to(Vector2(Target.position.x, self.position.y))) > 40:
+		self.position.x = move_toward(self.position.x, Target.position.x, 5)
+	if floor(self.position.distance_to(Vector2(Target.position.y, self.position.y))) > 30:
+		self.position.y = move_toward(self.position.y, Target.position.y, 10)
+
+# Calculating the gridnapped position
+#func desired_position() -> Vector2:
+	#return (Target.global_position / screen_size).floor() * screen_size + screen_size/2
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,15 +1,41 @@
 extends Node2D
 
+@export var gameVersion: String = "HyperCube 0.0.8a"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	loadMenu()
 
-#git test!
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func loadMenu():
+	var mainMenu = load("res://scenes/menus/mainMenu.tscn").instantiate()
+	call_deferred("add_child", mainMenu)
+
+func startLevel(level):
+	loadScene(level)
+	$"HUD".reset()
+	if $mainMenu != null:
+		$mainMenu.queue_free()
+	$"HUD".show()
+
+func loadScene(scene):
+	var newScene = load(scene).instantiate()
+	call_deferred("add_child", newScene)
+	
 func exit():
-	$ScreenCamera/ColorRect/AnimationPlayer.play("fadeOut")
+	$HUD/ColorRect/AnimationPlayer.play("fadeOut")
 	await get_tree().create_timer(0.5).timeout
-	get_tree().change_scene_to_file("res://scenes/menus/mainMenu.tscn")
+	if get_node("/root/Main/Level") != null: #the node always exists when exit() is called
+											 #but theres a crash if i dont check first
+		get_node("/root/Main/Level").free()
+	elif get_node("/root/Main/EditorMain/Level") != null:
+		get_node("/root/Main/EditorMain/Level").free()
+	$HUD.hide()
+	loadMenu()
+
+func reload(node):
+	var filename = get_node(node).get_scene_file_path()
+	get_node(node).free()
+	startLevel(filename)
